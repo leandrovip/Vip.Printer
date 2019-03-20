a0af962172d45c05881f76a314fa550648157ee6
Leandro Ferreira <leandro@vipsolucoes.com>
2019-03-20 09:24:56 -0300

    Fix EscBema/QrCode

--- src/Vip.Printer/EscBemaCommands/QrCode.cs
+++ src/Vip.Printer/EscBemaCommands/QrCode.cs
@@ -6,46 +6,37 @@ using Vip.Printer.Interfaces.Command;
 
 namespace Vip.Printer.EscBemaCommands
 {
-	internal class QrCode : IQrCode
-	{
-		private static byte[] Size(QrCodeSize size)
-		{
-			switch (size)
-			{
-				case QrCodeSize.Size1:
-					return new byte[] {1};
-				case QrCodeSize.Size2:
-					return new byte[] {2};
-				default:
-					return new byte[] {0};
-			}
-		}
+    internal class QrCode : IQrCode
+    {
+        private static byte[] Size(QrCodeSize size)
+        {
+            return new[] {size.ToByte()};
+        }
 
-		private static IEnumerable<byte> StoreQr(string qrData, QrCodeSize size)
-		{
-			var length = qrData.Length;
-			var b = (byte) (length % 256);
-			var b2 = (byte) (length / 256);
+        private static IEnumerable<byte> StoreQr(string qrData, QrCodeSize size)
+        {
+            var length = qrData.Length;
+            var b = (byte) (length % 256);
+            var b2 = (byte) (length / 256);
 
-			return new byte[] {29, 107, 81}
-				.AddBytes(Size(size))
-				.AddBytes(new byte[] {8, 0, 1})
-				.AddBytes(new[] {b})
-				.AddBytes(new[] {b2})
-				.AddBytes(new byte[] {13, 10});
-		}
+            return new byte[] {29, 107, 81}
+                .AddBytes(Size(size))
+                .AddBytes(new byte[] {6, 0, 1})
+                .AddBytes(new[] {b})
+                .AddBytes(new[] {b2});
+        }
 
-		public byte[] Print(string qrData)
-		{
-			return Print(qrData, QrCodeSize.Size0);
-		}
+        public byte[] Print(string qrData)
+        {
+            return Print(qrData, QrCodeSize.Size0);
+        }
 
-		public byte[] Print(string qrData, QrCodeSize qrCodeSize)
-		{
-			var list = new List<byte>();
-			list.AddRange(StoreQr(qrData, qrCodeSize));
-			list.AddRange(Encoding.UTF8.GetBytes(qrData));
-			return list.ToArray();
-		}
-	}
+        public byte[] Print(string qrData, QrCodeSize qrCodeSize)
+        {
+            var list = new List<byte>();
+            list.AddRange(StoreQr(qrData, qrCodeSize));
+            list.AddRange(Encoding.UTF8.GetBytes(qrData));
+            return list.ToArray();
+        }
+    }
 }
